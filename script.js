25ef9e9c35c97ba5f13a3a68b0c0c09a97c43883
kareenap55 <kareenap1412@gmail.com>
2025-04-05 02:43:35 -0700

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 //your JS code here. If required.
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>Uppercase Input</title>
+  <link rel="stylesheet" href="style.css">
+</head>
+<body>
+
+  <label for="fname">Name:</label>
+  <input type="text" id="fname" name="fname">
+
+  <script src="script.js"></script>
+</body>
+</html>
